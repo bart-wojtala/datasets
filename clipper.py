a49891cdab6b0f712e1f17427fdd7525249f6237
bart-wojtala <bartlomiej.wojtala@gmail.com>
2020-08-06 16:45:06 +0200

    Adjust script to cut silences from audio and add 100ms silence at the end

--- clipper.py
+++ clipper.py
@@ -14,10 +14,24 @@ def clip_files(directory):
     silence = AudioSegment.silent(duration = 100)
     files = [f for f in listdir(main_directory) if isfile(join(main_directory, f))]
     for f in files:
-        input_audio = AudioSegment.from_wav(join(main_directory, f))
-        output_audio = input_audio + silence
+        sound = AudioSegment.from_file(join(main_directory, f), format="wav")
+
+        start_trim = detect_leading_silence(sound)
+        end_trim = detect_leading_silence(sound.reverse())
+
+        duration = len(sound)    
+        trimmed_sound = sound[start_trim:duration - end_trim]
+        output_audio = trimmed_sound + silence
         output_audio.export(join(clean_directory, f), format="wav")
     
+def detect_leading_silence(sound, silence_threshold=-50.0, chunk_size=10):
+    trim_ms = 0 # ms
+    assert chunk_size > 0 # to avoid infinite loop
+    while sound[trim_ms:trim_ms + chunk_size].dBFS < silence_threshold and trim_ms < len(sound):
+        trim_ms += chunk_size
+
+    return trim_ms
+
 
 if __name__ == "__main__":
-    clip_files("/datasets/trump/wavs")
+    clip_files("/datasets/trump/wavs") # Set wavs directory here
