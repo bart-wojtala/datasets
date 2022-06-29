080ec7ed9ea3a2beb36464f9fb9e2c3d8afd6ef2
bart3s <bartlomiej.wojtala@gmail.com>
2022-06-29 09:17:03 +0200

    Add script for copying Gothic wav files based on transcript

---
+++ copy_wavs.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+
+
+if __name__ == '__main__':
+    main_directory = 'datasets/flowtron/Gothic 1+2/'
+    wavs_subdirectories = ['Gothic 1/wavs/', 'Gothic 2/wavs/']
+    target_directory = 'datasets/flowtron/wavs/'
+    file_list = 'gothic_list.txt'
+
+    with open(file_list, 'r', encoding='utf-8') as f:
+        content = f.readlines()
+
+    for i, line in enumerate(content):
+        content[i] = line.split('|')[0][5:].lower()
+
+    for directory in wavs_subdirectories:
+        dataset_directory = main_directory + directory
+        for filename in os.listdir(dataset_directory):
+            if filename.lower() in content:
+                source = os.path.join(
+                    os.getcwd(), dataset_directory + filename)
+                target = os.path.join(os.getcwd(), target_directory)
+                shutil.copy(source, target)
